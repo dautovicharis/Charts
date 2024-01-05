7a0a8a4b19cbe5e435783336014edaf9f6d05d8a
Haris Dautovic <haris.dautovic.dev@gmail.com>
2024-01-05 22:29:06 +0100

    Update app demo

--- app/src/main/java/com/hd/charts/MainActivity.kt
+++ app/src/main/java/com/hd/charts/MainActivity.kt
@@ -3,45 +3,66 @@ package com.hd.charts
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.hd.charts.ui.theme.ChartsTheme
+import androidx.compose.ui.unit.dp
+import com.hd.charts.lib.R
+import com.hd.charts.lib.piechart.PieChartView
+import com.hd.charts.ui.theme.HDChartViewTheme
+import com.hd.charts.lib.R as libR
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            ChartsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            HDChartViewTheme {
+                View()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+private fun View() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background),
+        contentAlignment = Alignment.Center
+    ) {
+
+        Column(
+            modifier = Modifier
+                .width(200.dp)
+                .wrapContentHeight()
+
+
+        ) {
+            PieChartView(
+                data = listOf(
+                    8.0, 23.0, 54.0, 32.0, 12.0, 37.0
+                ),
+                title = stringResource(id = libR.string.pie_chart),
+                legend = stringResource(id = R.string.legend)
+            )
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun GreetingPreview() {
-    ChartsTheme {
-        Greeting("Android")
+fun Preview() {
+    HDChartViewTheme() {
+        View()
     }
 }
