0656fa618b65f70dd774ee0cd2899bbcb603e0cd
Haris Dautovic <haris.dautovic.dev@gmail.com>
2024-01-21 14:47:58 +0100

    Update app demo
    Add line chart

--- app/src/main/java/com/hd/charts/app/MainActivity.kt
+++ app/src/main/java/com/hd/charts/app/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.MaterialTheme
@@ -19,6 +21,8 @@ import androidx.compose.ui.unit.dp
 import com.hd.charts.R
 import com.hd.charts.piechart.PieChartView
 import com.hd.charts.app.ui.theme.AppTheme
+import com.hd.charts.linechart.Defaults.lineChartStyle
+import com.hd.charts.linechart.LineChartView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -54,6 +58,17 @@ private fun View() {
                 title = stringResource(id = R.string.pie_chart),
                 legend = stringResource(id = R.string.legend)
             )
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            LineChartView(
+                data = listOf(
+                    8, 23, 54, 32, 12, 37, 7, 23, 43
+                ),
+                chartStyle = lineChartStyle(
+                    bezier = true,
+                    showPoints = false),
+                title = stringResource(id = R.string.line_chart))
         }
     }
 }
