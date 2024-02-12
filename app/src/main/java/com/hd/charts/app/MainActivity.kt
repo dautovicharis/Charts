edb4c02bb951ea898c8e67ff1250266f28014485
Haris Dautovic <haris.dautovic.dev@gmail.com>
2024-02-12 17:16:07 +0100

    App demo - add main menu icons

--- app/src/main/java/com/hd/charts/app/MainActivity.kt
+++ app/src/main/java/com/hd/charts/app/MainActivity.kt
@@ -5,89 +5,107 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.hd.charts.app.demo.bar.AddBarChartDemo
 import com.hd.charts.app.demo.line.AddLineChartDemo
 import com.hd.charts.app.demo.multiline.AddMultiLineChartDemo
 import com.hd.charts.app.demo.pie.AddPieChartDemo
-import com.hd.charts.app.demo.bar.AddBarChartDemo
 import com.hd.charts.app.demo.stackedbar.AddStackedBarChartDemo
 import com.hd.charts.app.ui.theme.AppTheme
-import com.hd.charts.R as ChartsR
 
 class MainActivity : ComponentActivity() {
 
     private val charts = listOf(
-        ChartsR.string.pie_chart to ChartScreen.PieChartScreen.route,
-        ChartsR.string.line_chart to ChartScreen.LineChartScreen.route,
-        ChartsR.string.multi_line_chart to ChartScreen.MultiLineChartScreen.route,
-        ChartsR.string.bar_chart to ChartScreen.BarChartScreen.route,
-        ChartsR.string.bar_stacked_chart to ChartScreen.StackedBarChartScreen.route,
+        ChartScreen.PieChartScreen,
+        ChartScreen.LineChartScreen,
+        ChartScreen.MultiLineChartScreen,
+        ChartScreen.BarChartScreen,
+        ChartScreen.StackedBarChartScreen,
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AppTheme {
-                val navController = rememberNavController()
-                NavHost(
-                    navController = navController,
-                    startDestination = ChartScreen.MainScreen.route
-                ) {
-                    composable(ChartScreen.MainScreen.route) {
-                        View(charts) { (_, route) ->
-                            navController.navigate(route)
-                        }
-                    }
+            MainView()
+        }
+    }
 
-                    // Pie charts
-                    composable(ChartScreen.PieChartScreen.route) {
-                        AddPieChartDemo()
-                    }
-
-                    // Line charts
-                    composable(ChartScreen.LineChartScreen.route) { AddLineChartDemo() }
-                    composable(ChartScreen.BarChartScreen.route) { AddBarChartDemo() }
-
-                    // Bar charts
-                    composable(ChartScreen.StackedBarChartScreen.route) { AddStackedBarChartDemo() }
-                    composable(ChartScreen.MultiLineChartScreen.route) { AddMultiLineChartDemo() }
+    @Composable
+    private fun MainView() {
+        AppTheme {
+            val navController = rememberNavController()
+            NavHost(
+                navController = navController,
+                startDestination = ChartScreen.MainScreen.route
+            ) {
+                composable(ChartScreen.MainScreen.route) {
+                    View(items = charts, onItemClick = {
+                        navController.navigate(it.route)
+                    })
                 }
+
+                // Pie charts
+                composable(ChartScreen.PieChartScreen.route) {
+                    AddPieChartDemo()
+                }
+
+                // Line charts
+                composable(ChartScreen.LineChartScreen.route) { AddLineChartDemo() }
+                composable(ChartScreen.BarChartScreen.route) { AddBarChartDemo() }
+
+                // Bar charts
+                composable(ChartScreen.StackedBarChartScreen.route) { AddStackedBarChartDemo() }
+                composable(ChartScreen.MultiLineChartScreen.route) { AddMultiLineChartDemo() }
             }
         }
     }
 
     @Composable
-    private fun ChartTypeItem(item: Pair<Int, String>, onClick: () -> Unit) {
+    private fun ChartTypeItem(item: ChartScreen, onClick: () -> Unit) {
         Button(modifier = Modifier
             .fillMaxWidth()
             .padding(10.dp), onClick = {
             onClick()
         }) {
-            Text(
-                text = stringResource(id = item.first),
-                style = MaterialTheme.typography.titleLarge,
-                color = MaterialTheme.colorScheme.onSurface
-            )
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    painterResource(id = item.icon),
+                    contentDescription = stringResource(id = item.title)
+                )
+                Text(
+                    modifier = Modifier.padding(start = 10.dp),
+                    text = stringResource(id = item.title),
+                    style = MaterialTheme.typography.titleLarge,
+                    color = MaterialTheme.colorScheme.onSurface
+                )
+            }
         }
     }
 
     @Composable
-    private fun View(items: List<Pair<Int, String>>, onItemClick: (Pair<Int, String>) -> Unit) {
+    private fun View(items: List<ChartScreen>, onItemClick: (ChartScreen) -> Unit) {
         Box(
             modifier = Modifier
                 .fillMaxSize()
@@ -103,5 +121,11 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+    @Preview
+    @Composable
+    fun MainViewPreview() {
+        MainView()
     }
 }
--- app/src/main/java/com/hd/charts/app/Navigation.kt
+++ app/src/main/java/com/hd/charts/app/Navigation.kt
@@ -1,10 +1,32 @@
 package com.hd.charts.app
 
-sealed class ChartScreen(val route: String) {
-    data object MainScreen : ChartScreen("main")
-    data object PieChartScreen : ChartScreen("pieChart")
-    data object LineChartScreen : ChartScreen("lineChart")
-    data object MultiLineChartScreen : ChartScreen("multiLineChart")
-    data object BarChartScreen : ChartScreen("barChart")
-    data object StackedBarChartScreen : ChartScreen("stackedBarChart")
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import com.hd.charts.R
+import com.hd.charts.app.R as AppR
+
+sealed class ChartScreen(
+    val route: String,
+    @DrawableRes val icon: Int,
+    @StringRes val title: Int
+) {
+    object MainScreen {
+        const val route = "main"
+    }
+
+    data object PieChartScreen :
+        ChartScreen("pieChart", AppR.drawable.ic_pie_chart, R.string.pie_chart)
+
+    data object LineChartScreen :
+        ChartScreen("lineChart", AppR.drawable.ic_line_chart, R.string.line_chart)
+
+    data object MultiLineChartScreen :
+        ChartScreen("multiLineChart", AppR.drawable.ic_multi_line_chart, R.string.multi_line_chart)
+
+    data object BarChartScreen :
+        ChartScreen("barChart", AppR.drawable.ic_bar_chart, R.string.bar_chart)
+
+    data object StackedBarChartScreen :
+        ChartScreen("stackedBarChart", AppR.drawable.ic_stacked_bar_chart, R.string.bar_stacked_chart)
 }
+
