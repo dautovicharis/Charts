c81b06e0652f2c27ac95a3bf7a0c2697ed7b58f4
Haris Dautovic <haris.dautovic.dev@gmail.com>
2024-02-13 13:41:40 +0100

    Improve app demo
    - Support for multiple themes
    - Github icon
    - App and build version

--- app/src/main/java/com/hd/charts/app/MainActivity.kt
+++ app/src/main/java/com/hd/charts/app/MainActivity.kt
@@ -1,23 +1,37 @@
 package com.hd.charts.app
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.FilledIconToggleButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -31,6 +45,8 @@ import com.hd.charts.app.demo.multiline.AddMultiLineChartDemo
 import com.hd.charts.app.demo.pie.AddPieChartDemo
 import com.hd.charts.app.demo.stackedbar.AddStackedBarChartDemo
 import com.hd.charts.app.ui.theme.AppTheme
+import com.hd.charts.app.ui.theme.Theme
+import com.hd.charts.app.ui.theme.ThemeManager
 
 class MainActivity : ComponentActivity() {
 
@@ -51,16 +67,34 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     private fun MainView() {
-        AppTheme {
+        var selectedTheme by remember { mutableStateOf(ThemeManager.currentTheme) }
+
+        AppTheme(theme = selectedTheme) {
             val navController = rememberNavController()
             NavHost(
                 navController = navController,
                 startDestination = ChartScreen.MainScreen.route
             ) {
+
                 composable(ChartScreen.MainScreen.route) {
-                    View(items = charts, onItemClick = {
-                        navController.navigate(it.route)
-                    })
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(MaterialTheme.colorScheme.background),
+                    ) {
+                        AddGithubIcon()
+
+                        AddThemes(
+                            currentTheme = selectedTheme,
+                            themes = ThemeManager.themes
+                        ) {
+                            selectedTheme = it
+                        }
+
+                        AddMenuItems { chartItem ->
+                            navController.navigate(chartItem.route)
+                        }
+                    }
                 }
 
                 // Pie charts
@@ -75,6 +109,102 @@ class MainActivity : ComponentActivity() {
                 // Bar charts
                 composable(ChartScreen.StackedBarChartScreen.route) { AddStackedBarChartDemo() }
                 composable(ChartScreen.MultiLineChartScreen.route) { AddMultiLineChartDemo() }
+            }
+        }
+    }
+
+    @Composable
+    private fun AddMenuItems(selectedItem: (selected: ChartScreen) -> Unit) {
+        Column(
+            modifier = Modifier
+                .background(Color.Transparent)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            View(items = charts) { chartItem ->
+                selectedItem(chartItem)
+            }
+
+            Text(
+                modifier = Modifier
+                    .padding(10.dp)
+                    .align(Alignment.CenterHorizontally),
+                text = "Version: ${BuildConfig.VERSION_NAME}\n"
+                        + "Build: ${BuildConfig.VERSION_CODE}",
+                color = MaterialTheme.colorScheme.onBackground
+            )
+        }
+    }
+
+    @Composable
+    private fun AddGithubIcon() {
+        val githubUrl = stringResource(id = R.string.github_url)
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(MaterialTheme.colorScheme.background),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.End
+        ) {
+            IconButton(
+                modifier = Modifier
+                    .padding(20.dp)
+                    .size(35.dp),
+                onClick = {
+                    val intent =
+                        Intent(Intent.ACTION_VIEW, Uri.parse(githubUrl)).apply {
+                            flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                        }
+                    baseContext.startActivity(intent)
+                }
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_github),
+                    tint = MaterialTheme.colorScheme.onBackground,
+                    contentDescription = stringResource(id = R.string.github_url_content_description)
+                )
+            }
+        }
+    }
+
+    @Composable
+    private fun AddThemes(
+        currentTheme: Theme,
+        themes: List<Theme>,
+        selectedTheme: (selected: Theme) -> Unit
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Row {
+                LazyRow {
+                    items(items = themes) { theme ->
+                        val isSelectedTheme = currentTheme == theme
+                        FilledIconToggleButton(
+                            checked = isSelectedTheme,
+                            onCheckedChange = {
+                                selectedTheme(theme)
+                            },
+                            modifier = Modifier.padding(10.dp),
+                            content = {
+                                if (isSelectedTheme) {
+                                    Icon(
+                                        painter = painterResource(id = R.drawable.ic_check),
+                                        contentDescription = stringResource(id = R.string.themes_content_description)
+                                    )
+                                }
+                            },
+                            colors = IconButtonDefaults.filledIconToggleButtonColors(
+                                containerColor = theme.colors.md_theme_light_primary
+                            ),
+                            shape = CircleShape
+                        )
+                    }
+                }
             }
         }
     }
@@ -97,35 +227,28 @@ class MainActivity : ComponentActivity() {
                 Text(
                     modifier = Modifier.padding(start = 10.dp),
                     text = stringResource(id = item.title),
-                    style = MaterialTheme.typography.titleLarge,
-                    color = MaterialTheme.colorScheme.onSurface
+                    style = MaterialTheme.typography.titleLarge
+                )
+            }
+        }
+
+    }
+
+    @Composable
+    private fun View(items: List<ChartScreen>, onItemClick: (ChartScreen) -> Unit) {
+        LazyColumn {
+            items(items = items) { chartItem ->
+                ChartTypeItem(
+                    item = chartItem,
+                    onClick = { onItemClick(chartItem) }
                 )
             }
         }
     }
 
+    @Preview(showBackground = true)
     @Composable
-    private fun View(items: List<ChartScreen>, onItemClick: (ChartScreen) -> Unit) {
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(MaterialTheme.colorScheme.background),
-            contentAlignment = Alignment.Center
-        ) {
-            LazyColumn {
-                items(items = items) { chartItem ->
-                    ChartTypeItem(
-                        item = chartItem,
-                        onClick = { onItemClick(chartItem) }
-                    )
-                }
-            }
-        }
-    }
-
-    @Preview
-    @Composable
-    fun MainViewPreview() {
+    private fun MainViewPreview() {
         MainView()
     }
 }
