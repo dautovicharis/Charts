fae63fdf4094b477bf4c0968cd6a5a943f0cc1c8
Haris Dautovic <haris.dautovic.dev@gmail.com>
2024-02-15 11:07:09 +0100

    Update app version
    - Updated app and build version
    - Added charts version label on the Main screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.hd.charts.app"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
