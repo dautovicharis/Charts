f5162d2f3517891f05aa19da9d1138c0aba71e93
Haris Dautovic <haris.dautovic.dev@gmail.com>
2024-08-04 11:49:08 +0200

    Skip animation for the preview

---
+++ charts/src/commonTest/kotlin/io/github/dautovicharis/charts/unit/common/StateTest.kt
@@ -0,0 +1,32 @@
+package io.github.dautovicharis.charts.unit.common
+
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.runComposeUiTest
+import io.github.dautovicharis.charts.internal.common.composable.rememberAnimationState
+import io.github.dautovicharis.charts.internal.common.composable.rememberShowState
+import kotlin.test.Test
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+class StateTest {
+
+    @OptIn(ExperimentalTestApi::class)
+    @Test
+    fun rememberShowState_shouldReturnFalse() = runComposeUiTest {
+        setContent {
+            val result = rememberShowState()
+            assertFalse {
+                result.value
+            }
+        }
+    }
+
+    @OptIn(ExperimentalTestApi::class)
+    @Test
+    fun rememberAnimationState_shouldReturnZero() = runComposeUiTest {
+        setContent {
+            val result = rememberAnimationState()
+            assertTrue { result.value == 0f }
+        }
+    }
+}
