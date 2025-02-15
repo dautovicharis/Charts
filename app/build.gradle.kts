a7b80c208a34437bdc7173eeea0d51ebb4ec9d62
Haris Dautovic <haris.dautovic.dev@gmail.com>
2025-02-15 16:35:55 +0100

    Use Koin bom

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ kotlin {
             implementation(libs.androidx.lifecycle.runtime.compose)
             implementation(libs.androidx.navigation.compose)
             implementation(libs.androidx.lifecycle.viewmodel.compose)
+            implementation(libs.koin.core)
             implementation(libs.koin.compose.viewmodel)
         }
 
